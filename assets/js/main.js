64608bd1221949da5c8f0bfffd97e49279e60fcc
lucaso-silva <luc.osilva@gmail.com>
2023-03-26 13:06:21 -0300

    feat: function shownAnswer

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,27 @@
 const menuMobile = document.querySelector(".menu-hbg--img");
 const menuNav = document.querySelector(".nav");
+const questions = document.querySelectorAll(".faq-question");
+const answers = document.querySelectorAll(".faq-answer");
+
+
+//functions
+function shownAnswer(index) {
+    const answerSelected = answers[index];
+    const questionSelected = questions[index];
+
+    answerSelected.classList.toggle("hide");
+    questionSelected.classList.toggle("selected");
+}
+
+
+//events
 
 menuMobile.addEventListener("click", ()=>{
     menuNav.classList.toggle("nav-mobile");
-    console.log("fui clicado!")
+})
+
+questions.forEach((question, index) => {
+    question.addEventListener("click", ()=>{
+        shownAnswer(index);
+    })
 })
