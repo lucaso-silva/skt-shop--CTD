c48b35b3641513a285e77cce1a5be59675f3d734
lucaso-silva <luc.osilva@gmail.com>
2023-04-06 02:47:24 -0300

    style: input error msg layout adjustment

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,96 +11,97 @@ const inputEmail = document.querySelector(".input");
 const buttonEmail = document.querySelector(".subscribe");
 const errorMessage = document.querySelector(".error-message");
 
-let numItens = 0
-
+let numItens = 0;
 
 //functions
 
 function addItemToCart(numItens) {
-    const counterDisplay = document.querySelector(".counter");
+  const counterDisplay = document.querySelector(".counter");
 
-    counterDisplay.classList.remove("hide");
-    counterDisplay.textContent = numItens;
+  counterDisplay.classList.remove("hide");
+  counterDisplay.textContent = numItens;
 }
 
 function showTestimonial(index) {
-    const testimonialActive = allTestimonials[index]
-    const arrayTestimonials = Array.from(allTestimonials)
-    
-    const divTestimonialShow = arrayTestimonials.splice(index, 1)
-    
-    testimonialActive.classList.remove("hide");
-    arrayTestimonials.forEach(arrayTestimonial => arrayTestimonial.classList.add("hide"));
-    console.log(testimonialsBtns);
+  const testimonialActive = allTestimonials[index];
+  const arrayTestimonials = Array.from(allTestimonials);
+
+  const divTestimonialShow = arrayTestimonials.splice(index, 1);
+
+  testimonialActive.classList.remove("hide");
+  arrayTestimonials.forEach((arrayTestimonial) =>
+    arrayTestimonial.classList.add("hide")
+  );
+  console.log(testimonialsBtns);
 }
 
 function shownAnswer(index) {
-    const answerSelected = answers[index];
-    const questionSelected = questions[index];
-    const arrayAnswers = Array.from(answers);
-    const arrayQuestions = Array.from(questions);
+  const answerSelected = answers[index];
+  const questionSelected = questions[index];
+  const arrayAnswers = Array.from(answers);
+  const arrayQuestions = Array.from(questions);
 
-    const removeAnswerSelected = arrayAnswers.splice(index, 1);
-    const removeQuestionSelected = arrayQuestions.splice(index, 1);
+  const removeAnswerSelected = arrayAnswers.splice(index, 1);
+  const removeQuestionSelected = arrayQuestions.splice(index, 1);
 
-    answerSelected.classList.toggle("hide");
-    questionSelected.classList.toggle("selected");
+  answerSelected.classList.toggle("hide");
+  questionSelected.classList.toggle("selected");
 
-    arrayAnswers.forEach(answer => answer.classList.add("hide"))
-    arrayQuestions.forEach(question => question.classList.remove("selected"))
+  arrayAnswers.forEach((answer) => answer.classList.add("hide"));
+  arrayQuestions.forEach((question) => question.classList.remove("selected"));
 }
 
 function validateInput() {
-    const inputValidity = inputEmail.checkValidity();
-    
-    let message = ""
+  const inputValidity = inputEmail.checkValidity();
 
-    if(inputEmail.value === "") {
-        message = "Please insert a email address"
-    } else if (inputEmail.validity.typeMismatch) {
-        message = "This doesn't look like a valid email. A valid email must be like: name@email.com"
-    } else {
-        message = "";
-    }
+  let message = "";
 
-    if(!inputValidity) {
-        errorMessage.innerText = message;
-        errorMessage.classList.add("error-message--active");
-    } else {
-        errorMessage.classList.remove("error-message--active");
-        errorMessage.innerText = "";
-    }
+  if (inputEmail.value === "") {
+    message = "Please insert a email address";
+  } else if (inputEmail.validity.typeMismatch) {
+    message =
+      "This doesn't look like a valid email!";
+  } else {
+    message = "";
+  }
+
+  if (!inputValidity) {
+    errorMessage.innerText = message;
+    errorMessage.classList.add("error-message--active");
+  } else {
+    errorMessage.classList.remove("error-message--active");
+    errorMessage.innerText = "";
+  }
 }
-
 
 //events
 
-menuMobile.addEventListener("click", ()=>{
-    menuNav.classList.toggle("nav-mobile");
-})
+menuMobile.addEventListener("click", () => {
+  menuNav.classList.toggle("nav-mobile");
+});
 
-buyBtns.forEach(btn =>{
-    btn.addEventListener("click", ()=> {
-        numItens++
-        addItemToCart(numItens);
-    })
-})
+buyBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    numItens++;
+    addItemToCart(numItens);
+  });
+});
 
 questions.forEach((question, index) => {
-    question.addEventListener("click", ()=>{
-        shownAnswer(index);
-    })
-})
+  question.addEventListener("click", () => {
+    shownAnswer(index);
+  });
+});
 
 testimonialsBtns.forEach((testimonialBtn, index) => {
-    testimonialBtn.addEventListener("click", ()=> {
-        showTestimonial(index)
-    })
-})
+  testimonialBtn.addEventListener("click", () => {
+    showTestimonial(index);
+  });
+});
 
-buttonEmail.addEventListener("click", (e)=>{
-    e.preventDefault();
-    validateInput();
+buttonEmail.addEventListener("click", (e) => {
+  e.preventDefault();
+  validateInput();
 
-    inputEmail.value = "";
+  inputEmail.value = "";
 });
